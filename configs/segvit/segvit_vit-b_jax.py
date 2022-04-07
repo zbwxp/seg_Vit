864c21d5018c127ee427b5dc8af331990e3e8eef
zhangbowen17 <zhangbowen17@meituan.com>
2022-04-07 17:10:47 +0930

    jax and jx backbone

--- configs/segvit/segvit_vit-b_jax.py
+++ configs/segvit/segvit_vit-b_jax.py
@@ -1,16 +1,12 @@
 _base_ = [
-    '../_base_/models/seg_vit-b16.py',
-    '../_base_/datasets/ade20k.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_160k.py'
+    './segvit_vit-b_jx_512x512_160k_ade20k.py'
 ]
-out_indices = [5, 7, 11]
+checkpoint = './pretrained/vit_base_p16_384_20220308-96dfe169.pth'
 model = dict(
-    decode_head=dict(
-        loss_decode=dict(
-            type='ATMLoss', num_classes=150, dec_layers=len(out_indices), loss_weight=1.0),
-    )
+    pretrained=checkpoint,
 )
 
+# jax use different img norm cfg
 img_norm_cfg = dict(
     mean=[127.5, 127.5, 127.5], std=[127.5, 127.5, 127.5], to_rgb=True)
 crop_size = (512, 512)
@@ -46,19 +42,3 @@ data = dict(
     train=dict(pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
-
-optimizer = dict(_delete_=True, type='AdamW', lr=0.00002, betas=(0.9, 0.999), weight_decay=0.01,
-                 paramwise_cfg=dict(custom_keys={'norm': dict(decay_mult=0.),
-                                                 'ln': dict(decay_mult=0.),
-                                                 'head': dict(lr_mult=10.),
-                                                 }))
-#
-optimizer_config = dict(
-    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
-
-lr_config = dict(_delete_=True, policy='poly',
-                 warmup='linear',
-                 warmup_iters=1500,
-                 warmup_ratio=1e-6,
-                 power=1.0, min_lr=0.0, by_epoch=False)
-
