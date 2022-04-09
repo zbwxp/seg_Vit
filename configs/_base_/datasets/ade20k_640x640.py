332a40f574ad02c6f636982a46d3845024718f0e
zhangbowen17 <zhangbowen17@meituan.com>
2022-04-09 18:25:54 +0930

    fix minor

--- configs/_base_/datasets/ade20k_640x640.py
+++ configs/_base_/datasets/ade20k_640x640.py
@@ -7,7 +7,7 @@ crop_size = (640, 640)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=True),
-    dict(type='Resize', img_scale=(2048, 512), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(2048, 640), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
