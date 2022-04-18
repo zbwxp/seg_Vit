14365d141bf969a61a0da21a88d153201307a803
zhangbowen17 <zhangbowen17@meituan.com>
2022-04-18 19:24:40 +0930

    mae using one loss only

--- configs/segvit/segvit_vit-b_mae.py
+++ configs/segvit/segvit_vit-b_mae.py
@@ -6,6 +6,11 @@ model = dict(
     pretrained=checkpoint,
     backbone=dict(
         type="vit_fixpos",
+    ),
+    decode_head=dict(
+        num_layers=1,
+        loss_decode=dict(
+            type='ATMLoss', num_classes=150, dec_layers=1, loss_weight=1.0),
     )
 )
 find_unused_parameters=True
