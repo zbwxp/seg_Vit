438642e485c2e1eca760eff88962711f0bc1a912
zhangbowen17 <zhangbowen17@meituan.com>
2022-04-19 14:50:07 +0930

    fix dist test

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -6,4 +6,4 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=4 --master_port=$((RANDOM + 10000)) \
-    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:3}
+    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --eval mIoU  --launcher pytorch ${@:3}
