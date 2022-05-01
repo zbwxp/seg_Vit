8026d7018ca3d8ea722c12f70a531728b8bcca69
zhangbowen17 <zhangbowen17@meituan.com>
2022-05-01 17:17:14 +0930

    wd 0.02 lr power 1.0 to 2.0

--- configs/segvit/segvit_vit-l_jax_480x480_80k_pc.py
+++ configs/segvit/segvit_vit-l_jax_480x480_80k_pc.py
@@ -71,7 +71,7 @@ data = dict(
     test=dict(pipeline=test_pipeline))
 
 
-optimizer = dict(_delete_=True, type='AdamW', lr=0.00002, betas=(0.9, 0.999), weight_decay=0.05,
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00002, betas=(0.9, 0.999), weight_decay=0.02,
                  paramwise_cfg=dict(custom_keys={'norm': dict(decay_mult=0.),
                                                  'ln': dict(decay_mult=0.),
                                                  'head': dict(lr_mult=10.),
@@ -84,5 +84,5 @@ lr_config = dict(_delete_=True, policy='poly',
                  warmup='linear',
                  warmup_iters=1500,
                  warmup_ratio=1e-6,
-                 power=1.0, min_lr=0.0, by_epoch=False)
+                 power=2.0, min_lr=0.0, by_epoch=False)
 
