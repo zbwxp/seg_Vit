8eaff30abf99fa7ecb4c0f1b8af088c1be640d35
zhangbowen17 <zhangbowen17@meituan.com>
2022-05-02 14:07:13 +0930

    abs pos, (no dial yet) optimizer 0.00002 lr power 2.0

--- configs/segvit/swin/segvit_swin_large_all.py
+++ configs/segvit/swin/segvit_swin_large_all.py
@@ -8,13 +8,13 @@ img_size = 640
 model = dict(
     pretrained=checkpoint,
     backbone=dict(
-        pretrain_img_size=384,
+        pretrain_img_size=img_size,
         embed_dims=192,
         window_size=12,
         depths=[2, 2, 18, 2],
         num_heads=[6, 12, 24, 48],
         use_abs_pos_embed=True,
-        strides=(4, 2, 2, 1),
+        # strides=(4, 2, 2, 1),
     ),
     neck=dict(
         transformer=False,
@@ -33,7 +33,7 @@ model = dict(
     test_cfg=dict(mode='slide', crop_size=(640, 640), stride=(608, 608)),
 )
 data = dict(samples_per_gpu=4,)
-optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00002, betas=(0.9, 0.999), weight_decay=0.01,
                  paramwise_cfg=dict(custom_keys={'norm': dict(decay_mult=0.),
                                                  'linear': dict(decay_mult=0.),
                                                  'head': dict(lr_mult=10.),
@@ -46,5 +46,5 @@ lr_config = dict(_delete_=True, policy='poly',
                  warmup='linear',
                  warmup_iters=1500,
                  warmup_ratio=1e-6,
-                 power=1.0, min_lr=0.0, by_epoch=False)
+                 power=2.0, min_lr=0.0, by_epoch=False)
 find_unused_parameters = False
