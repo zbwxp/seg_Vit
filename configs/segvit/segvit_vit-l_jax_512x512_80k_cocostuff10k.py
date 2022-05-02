62feedb984d5b4cfd748ecf011c1a6159a8f24af
zhangbowen17 <zhangbowen17@meituan.com>
2022-05-02 23:13:06 +0930

    change coco stuff lr pow 1.0 to 2.0

--- configs/segvit/segvit_vit-l_jax_512x512_80k_cocostuff10k.py
+++ configs/segvit/segvit_vit-l_jax_512x512_80k_cocostuff10k.py
@@ -82,5 +82,5 @@ lr_config = dict(_delete_=True, policy='poly',
                  warmup='linear',
                  warmup_iters=1500,
                  warmup_ratio=1e-6,
-                 power=1.0, min_lr=0.0, by_epoch=False)
+                 power=2.0, min_lr=0.0, by_epoch=False)
 
