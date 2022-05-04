b79a98c44b41a2d9f31d57385b50c1dd2f75a556
zhangbowen17 <zhangbowen17@meituan.com>
2022-05-04 17:34:35 +0930

    minor fix workers = 2

--- configs/_base_/datasets/pascal_context_59.py
+++ configs/_base_/datasets/pascal_context_59.py
@@ -36,7 +36,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=2,
-    workers_per_gpu=0,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         data_root=data_root,
